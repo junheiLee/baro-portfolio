40d89070080b123b696e29809b4261b2224fd4b4
junheiLee <jun2ee22@naver.com>
2024-05-21 10:33:24 +0900

    Feat: 로그인 체크 필터 -> 로그인 체크 인터셉터로 변경

--- src/main/java/com/baro/portfolio/config/WebConfig.java
+++ src/main/java/com/baro/portfolio/config/WebConfig.java
@@ -1,12 +1,10 @@
 package com.baro.portfolio.config;
 
 import com.baro.portfolio.web.argumentresolver.CurrentAccountArgumentResolver;
-import com.baro.portfolio.web.filter.CheckLoginFilter;
-import jakarta.servlet.Filter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
+import com.baro.portfolio.web.interceptor.CheckAccountInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -19,14 +17,15 @@ public class WebConfig implements WebMvcConfigurer {
         resolvers.add(new CurrentAccountArgumentResolver());
     }
 
-    @Bean
-    public FilterRegistrationBean checkLoginFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-
-        filterRegistrationBean.setFilter(new CheckLoginFilter());
-        filterRegistrationBean.setOrder(1);
-        filterRegistrationBean.addUrlPatterns("/*");
-
-        return filterRegistrationBean;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CheckAccountInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/", "/users/sign-up",
+                        "/account/sign-in", "account/sign-out",
+                        "/css/**", "/error"
+                );
     }
 }
